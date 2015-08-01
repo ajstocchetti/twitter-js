363dbdb2a26331135afc82a808bbe624398d6763
tackypinkflamingos <ajstocchetti@gmail.com>
2015-08-01 12:53:14 -0400

    gitignore

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require("express");
 var app = express();
 var morgan = require("morgan");
+var swig = require('swig');
+
+swig.setDefaults({ cache: false });
 
 app.use(morgan('dev'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', process.cwd() + "/views");
+
+console.log(process.cwd())
 
 app.get('/', function(req, res) {
-   res.send("Hello Andy.");
-})
+   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+   res.render( 'index', {title: 'Hall of Fame', people: people } );
 
+})
 
 
 
